52c175213c7b0e6f8b8929af7b1427fffd0756d1
Thaerohde <thaerohde@gmail.com>
2018-03-24 21:50:10 +0100

    add spring files

---
+++ src/main/java/refugeescode/at/piratesOfTheCursedBean/PiratesOfTheCursedBeanApplication.java
@@ -0,0 +1,12 @@
+package refugeescode.at.piratesOfTheCursedBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiratesOfTheCursedBeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiratesOfTheCursedBeanApplication.class, args);
+	}
+}
---
+++ src/test/java/refugeescode/at/piratesOfTheCursedBean/PiratesOfTheCursedBeanApplicationTests.java
@@ -0,0 +1,16 @@
+package refugeescode.at.piratesOfTheCursedBean;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PiratesOfTheCursedBeanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
